aea3827725cff2bdd0440334ae86e99277c7f719
Derp Princess <nekokagamines@gmail.com>
2023-05-12 15:52:23 -0500

    add npm packages + fix so page will render, ready to take skills test

--- skills-test-react/src/App.test.js
+++ skills-test-react/src/App.test.js
@@ -1,6 +1,13 @@
 import { render, screen } from '@testing-library/react';
 import App from './App';
 
+describe('App', () => {
+  it('renders without crashing', () => {
+    render(<App />);
+  }
+  );
+ }
+
 test('renders learn react link', () => {
   render(<App />);
   const linkElement = screen.getByText(/learn react/i);
--- skills-test-react/src/BasicForm.js
+++ skills-test-react/src/BasicForm.js
@@ -1,26 +1,26 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import { Formik, Field, Form } from 'formik';
-import { Button } from '@mui/material';
+import React from "react";
+import ReactDOM from "react-dom";
+import { Formik, Field, Form } from "formik";
+import { Button } from "@mui/material";
 
 const BasicForm = () => (
   <div>
     <h1>TODO</h1>
     <Formik
       initialValues={{
-        firstName: '',
-        lastName: '',
-        email: '',
+        firstName: "",
+        lastName: "",
+        email: "",
       }}
       onSubmit={(values) => {}}
     >
       <Form>
         <label htmlFor="todo">Add ToDo </label>
         <Field id="todo" name="Add ToDo" placeholder="TextHere" />
-        <button type="submit">Submit</button>
+        <Button type="submit">Submit</Button>
       </Form>
     </Formik>
   </div>
 );
 
-export default BasicForm
+export default BasicForm;
