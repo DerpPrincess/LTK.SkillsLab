380a47d3acdf801cb5e0988b664d98c041cd12ee
Derp Princess <nekokagamines@gmail.com>
2023-05-14 12:54:37 -0500

    added basic mongoose functionality with requested schema

--- MockAPILab/index.js
+++ MockAPILab/index.js
@@ -1,7 +1,38 @@
+require("dotenv").config();
 const express = require("express");
 const app = express();
 const port = 3001;
 const isLocal = true;
+const mongoose = require("mongoose");
+const schema = mongoose.Schema;
+
+const userSchema = new schema(
+  {
+    loanId: Number,
+    borrowers: [
+      {
+        pairId: Number,
+        firstName: String,
+        lastName: String,
+        phone: String,
+      },
+    ],
+  },
+  { collection: "userData" }
+);
+
+const userData = mongoose.model("userData", userSchema);
+
+//connect to mongoDB
+mongoose.connect(process.env.MONGO_URI, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+});
+
+//check connection
+const db = mongoose.connection;
+db.on("error", console.error.bind(console, "connection error:"));
+db.once("open", () => console.log("connected to mongoDB"));
 
 app.get("/", (req, res) => {
   res.json({
